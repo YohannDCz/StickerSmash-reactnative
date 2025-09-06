626ee5604631a1e5784d5daca58f5a9821509fdf
Yohann Di Crescenzo <yohanndcz@gmail.com>
2025-09-06 15:02:35 +0200

    - scaffold - main screen (1/3)

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,15 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Link, Stack } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function NotFoundScreen() {
+export default function Index() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
+      <Stack.Screen name="not-found" options={{ title: "Not found" }} />
+      <View
+        style={styles.container}
+      >
+        <Link href="/" style={styles.button}>Go back to home screen!</Link>
+      </View >
     </>
   );
 }
@@ -21,12 +17,13 @@ export default function NotFoundScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e"
   },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+  button: {
+    fontSize: 14,
+    textDecorationLine: "underline",
+    color: "#fff",
+  }
+})
